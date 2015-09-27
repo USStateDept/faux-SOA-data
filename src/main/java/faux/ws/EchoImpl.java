554a38c8696924a67e353b07edd0ec50e2397a58
Jason Pyeron <jpyeron@pdinc.us>
2015-09-27 08:57:45 -0400

    logging

--- src/main/java/faux/ws/EchoImpl.java
+++ src/main/java/faux/ws/EchoImpl.java
@@ -1,6 +1,10 @@
 package faux.ws;
 
+import java.util.logging.Level;
+
 import javax.jws.WebService;
+
+import lombok.extern.java.Log;
 
 import org.apache.cxf.interceptor.InInterceptors;
 import org.springframework.stereotype.Component;
@@ -9,18 +13,21 @@ import org.springframework.stereotype.Component;
 @Component
 //@EndpointProperties({@EndpointProperty(key="faultStackTraceEnabled",value="true"),@EndpointProperty(key="exceptionMessageCauseEnabled",value="true")})
 @InInterceptors (interceptors = {"faux.ws.Int" })
+@Log
 public class EchoImpl implements Echo
 {
 
 	@Override
 	public String echoText(String msg)
 	{
+		log.log(Level.INFO,"msg={0}",msg);
 		return msg;
 	}
 
 	@Override
 	public Object echoXML(Object msg)
 	{
+		log.log(Level.INFO,"msg={0}",msg);
 		//http://stackoverflow.com/questions/17018714/what-is-the-simplest-way-to-extract-an-xml-node-for-jaxb-unmarshal
 		//http://docs.oracle.com/javaee/6/tutorial/doc/bnazc.html
 		//http://stackoverflow.com/questions/4212608/getting-raw-xml-parameter-in-jax-ws-webservice-method
--- src/main/java/faux/ws/Int.java
+++ src/main/java/faux/ws/Int.java
@@ -2,6 +2,9 @@ package faux.ws;
 
 import java.util.logging.Level;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.wsdl.extensions.http.HTTPAddress;
+
 import lombok.extern.java.Log;
 
 import org.apache.cxf.interceptor.Fault;
@@ -15,12 +18,60 @@ public class Int extends AbstractPhaseInterceptor<Message>
 	public Int()
 	{
 		super(Phase.UNMARSHAL);
+		log.log(Level.INFO,"Constructed");
 	}
 
 	@Override
 	public void handleMessage(Message message) throws Fault
 	{
 		log.log(Level.INFO,"message={0}",message);
+		
+		//Sep 27, 2015 8:27:43 AM faux.ws.Int handleMessage
+		//INFO: message={
+		//org.apache.cxf.message.MessageFIXED_PARAMETER_ORDER=false, 
+		//http.base.path=http://tomcat.poc.statedeptproject.com/faux-0.0.1-SNAPSHOT, 
+		//HTTP.REQUEST=org.apache.catalina.connector.RequestFacade@5927e0e2, 
+		//org.apache.cxf.transport.Destination=org.apache.cxf.transport.servlet.ServletDestination@36e19ac2, 
+		//HTTP.CONFIG=org.apache.catalina.core.StandardWrapperFacade@4678b9c, 
+		//org.apache.cxf.binding.soap.SoapVersion=org.apache.cxf.binding.soap.Soap11@1bac0a76, 
+		//org.apache.cxf.message.Message.QUERY_STRING=null, 
+		//org.apache.cxf.message.Message.ENCODING=UTF-8, 
+		//HTTP.CONTEXT=org.apache.catalina.core.ApplicationContextFacade@7d6ac911, 
+		//Content-Type=text/xml;charset=UTF-8, 
+		//org.apache.cxf.security.SecurityContext=org.apache.cxf.transport.http.AbstractHTTPDestination$2@3d0b5fd5, 
+		//org.apache.cxf.message.Message.PROTOCOL_HEADERS=
+		//{
+		// accept-encoding=[gzip,deflate],
+		// Cache-Control=[max-age=259200], 
+		// connection=[keep-alive], 
+		// Content-Length=[298], 
+		// content-type=[text/xml;charset=UTF-8], 
+		// host=[tomcat.poc.statedeptproject.com], 
+		// SOAPAction=[""], 
+		// user-agent=[Apache-HttpClient/4.1.1 (java 1.5)], 
+		// Via=[1.1 gateway.1.internal.pdinc.us:3128 (squid/2.6.STABLE21)], 
+		// X-Forwarded-For=[192.168.4.12]}, 
+		// org.apache.cxf.request.url=http://tomcat.poc.statedeptproject.com/faux-0.0.1-SNAPSHOT/cxf/services/echo, 
+		// Accept=null, 
+		// org.apache.cxf.request.uri=/faux-0.0.1-SNAPSHOT/cxf/services/echo, 
+		// org.apache.cxf.message.Message.PATH_INFO=/faux-0.0.1-SNAPSHOT/cxf/services/echo, 
+		// org.apache.cxf.transport.https.CertConstraints=null, 
+		// HTTP.RESPONSE=org.apache.catalina.connector.ResponseFacade@7fe423ef, 
+		// org.apache.cxf.headers.Header.list=[], 
+		// org.apache.cxf.request.method=POST, 
+		// org.apache.cxf.async.post.response.dispatch=true, 
+		// org.apache.cxf.message.Message.IN_INTERCEPTORS=[org.apache.cxf.transport.https.CertConstraintsInterceptor@7334275a], 
+		// HTTP_CONTEXT_MATCH_STRATEGY=stem, 
+		// http.service.redirection=null, 
+		// org.apache.cxf.message.Message.BASE_PATH=/faux-0.0.1-SNAPSHOT/cxf/services/echo, 
+		// org.apache.cxf.configuration.security.AuthorizationPolicy=null
+		//}
+		
+		HttpServletRequest req=(HttpServletRequest) message.get("HTTP.REQUEST");
+		log.log(Level.INFO,"request={0}",req);
+
+		
+		
 	}
 
 }
